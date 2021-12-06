ba93958b8b87dd8f380edff044fe126e3fcf2513
ruan.ramalho <ruanmbramalho@gmail.com>
2021-12-06 10:46:50 -0300

    feat: first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/lib/locale/pt_BR';
+import App from './App';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={ptBR}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
